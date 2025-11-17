afea64f50d71dda8caed63ab124e564d04cd96b2
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-17 14:29:42 +0545

    worked on admin.py of Users for making sure custom User model is displayed and managed in the Django admin

--- kishan_sathi_backend/Users/admin.py
+++ kishan_sathi_backend/Users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "full_name", "role", "is_active", "is_doctor_verified")
+    list_filter = ("role", "is_active", "is_doctor_verified", "is_staff")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("full_name", "phone_number", "role", "is_doctor_verified")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "full_name", "role", "password1", "password2", "is_staff", "is_superuser"),
+            },
+        ),
+    )
+    search_fields = ("email", "full_name")
