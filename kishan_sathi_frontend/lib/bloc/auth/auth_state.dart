8093688c5a4f2c027b4eb9636e35b5679db49bff
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-23 23:33:46 +0545

    add AuthState classes for handling authentication states in Bloc

---
+++ kishan_sathi_frontend/lib/bloc/auth/auth_state.dart
@@ -0,0 +1,42 @@
+import 'package:equatable/equatable.dart';
+import '../../models/user_model.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AuthInitial extends AuthState {
+  const AuthInitial();
+}
+
+class AuthLoading extends AuthState {
+  const AuthLoading();
+}
+
+class AuthSuccess extends AuthState {
+  final String token;
+  final UserModel user;
+  final String message;
+
+  const AuthSuccess({
+    required this.token,
+    required this.user,
+    required this.message,
+  });
+
+  @override
+  List<Object?> get props => [token, user, message];
+}
+
+class AuthFailure extends AuthState {
+  final String error;
+
+  const AuthFailure({required this.error});
+
+  @override
+  List<Object?> get props => [error];
+}
+
