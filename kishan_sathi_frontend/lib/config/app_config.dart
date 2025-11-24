900ffb80ce2761b5a71aeab28fad19b662484fbc
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-24 13:36:15 +0545

    Worked on app config for backend integration for login and registration endpoint for different users including doctor registeration

--- kishan_sathi_frontend/lib/config/app_config.dart
+++ kishan_sathi_frontend/lib/config/app_config.dart
@@ -1,13 +1,23 @@
 class AppConfig {
-  static const String baseUrl = 'http://127.0.0.1:8000/api';
+  // static const String baseUrl = 'http://127.0.0.1:8000/api';
   static const Duration requestTimeout = Duration(seconds: 30);
+  static const String baseUrl = 'https://kishansathi.onrender.com/api';
   
   // API Endpoints
   static const String loginEndpoint = '/auth/login/';
   static const String registerEndpoint = '/auth/register/';
+  static const String doctorRegisterEndpoint = 'auth/register/doctor/';
+  
   
   // SharedPreferences Keys
   static const String authTokenKey = 'auth_token';
   static const String userRoleKey = 'user_role';
+
+  //timeout duration
+  static const Duration timeout = Duration(seconds: 30);
+
+  // for full URL for endpoints
+  static String getUrl(String endpoint) => '$baseUrl$endpoint';
+
 }
 
