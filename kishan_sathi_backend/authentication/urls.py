a65c02d5a71c545614785abed4eadd946e890ec1
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-24 23:07:56 +0545

    updated urls.py of authentication app

--- kishan_sathi_backend/authentication/urls.py
+++ kishan_sathi_backend/authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from .views import login_view, register_view, doctor_register_view, logout_view
 
-from .views import LoginView, RegisterView, DoctorRegisterView
+app_name = 'authentication'
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="auth-register"),
-    path("register/doctor/", DoctorRegisterView.as_view(), name="auth-doctor-register"),
-    path("login/", LoginView.as_view(), name="auth-login"),
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('register/doctor/', doctor_register_view, name='doctor-register'),
+    path('logout/', logout_view, name='logout'),
 ]
 
