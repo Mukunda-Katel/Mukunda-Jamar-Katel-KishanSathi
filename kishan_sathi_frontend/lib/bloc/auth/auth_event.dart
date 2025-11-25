9032c024cb7c828768d239335ad484275fea1752
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-25 19:51:57 +0545

    Updated the auth event for authentication of doctor

--- kishan_sathi_frontend/lib/bloc/auth/auth_event.dart
+++ kishan_sathi_frontend/lib/bloc/auth/auth_event.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'dart:io'; 
 
 abstract class AuthEvent extends Equatable {
   const AuthEvent();
@@ -39,6 +40,41 @@ class RegisterRequested extends AuthEvent {
 
   @override
   List<Object?> get props => [fullName, email, phoneNumber, password, role];
+}
+
+// UPDATED: Doctor Registration Event with certificate file
+class DoctorRegisterRequested extends AuthEvent {
+  final String fullName;
+  final String email;
+  final String phoneNumber;
+  final String password;
+  final String specialization;
+  final int experienceYears;
+  final String licenseNumber;
+  final File certificateFile; 
+
+  const DoctorRegisterRequested({
+    required this.fullName,
+    required this.email,
+    required this.phoneNumber,
+    required this.password,
+    required this.specialization,
+    required this.experienceYears,
+    required this.licenseNumber,
+    required this.certificateFile, 
+  });
+
+  @override
+  List<Object?> get props => [
+        fullName,
+        email,
+        phoneNumber,
+        password,
+        specialization,
+        experienceYears,
+        licenseNumber,
+        certificateFile, 
+      ];
 }
 
 class LogoutRequested extends AuthEvent {
