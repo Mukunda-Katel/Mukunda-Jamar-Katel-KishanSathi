4025c7b242f2fd8e1ecbd3f467d4bf3e0a22ca6e
Mukunda-Katel <mukundakatel@gmail.com>
2025-11-30 21:33:00 +0545

    Created urls.py of project for farmers add product

--- kishan_sathi_backend/kishan_sathi_backend/urls.py
+++ kishan_sathi_backend/kishan_sathi_backend/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('authentication.urls')),
+    path('api/auth/', include('authentication.urls')),    
+    path('api/farmer/', include('farmer.urls')),
+    
 ]
 
 # Serve media files in development
