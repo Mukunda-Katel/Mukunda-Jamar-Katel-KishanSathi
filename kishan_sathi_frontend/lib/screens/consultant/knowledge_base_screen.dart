21f08960f4f16cb3f718722df4eb41dbb8f61d70
Mukunda-Katel <mukundakatel@gmail.com>
2026-03-22 12:29:17 +0545

    Worked on knowledge base screen

---
+++ kishan_sathi_frontend/lib/screens/consultant/knowledge_base_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KnowledgeBaseScreen extends StatelessWidget {
+  const KnowledgeBaseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Knowledge Base')),
+      body: const Center(
+        child: Text('Knowledge articles will appear here.'),
+      ),
+    );
+  }
+}
